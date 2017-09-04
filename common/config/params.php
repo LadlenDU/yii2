1b5c84a82beb2953fbf17b9cd1adc5e168f1c543
Ladlen <TwilightTower@mail.ru>
2017-09-04 22:19:52 +0300

    config changes

--- common/config/params.php
+++ common/config/params.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'adminEmail' => 'admin@example.com',
-    'supportEmail' => 'support@example.com',
+    'adminEmail' => 'TwilightTower@mail.ru',
+    'supportEmail' => 'TwilightTower@mail.ru',
     'user.passwordResetTokenExpire' => 3600,
 ];
