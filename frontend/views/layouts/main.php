3c442231e92366204575954aa97340658d0b85a5
Ladlen <TwilightTower@mail.ru>
2017-09-12 16:15:18 +0300

    main page html/css

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -87,16 +87,42 @@ AppAsset::register($this);
         ]) */ ?>
         <?= Alert::widget() ?>
         <? /*= $content */ ?>
+
+        <div class="row middle-ad-block">
+            <section class="col-md-6">разработка сайтов</section>
+            <section class="col-md-6">реклама в интернете</section>
+        </div>
+
+        <section class="row">
+            <div class="col-md-3 announcement">Анонс 1</div>
+            <div class="col-md-3 announcement">Анонс 2</div>
+            <div class="col-md-3 announcement">Анонс 3</div>
+            <div class="col-md-3 announcement">Анонс 4</div>
+        </section>
+
+        <div class="row">
+            <section class="col-md-7" style="background-color: #eee;">
+                Обращение к посетителю:<br>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vestibulum fringilla nulla non fringilla. Aenean eu urna sit amet eros euismod elementum. Aliquam tempor auctor lacus. Aliquam ornare ligula vel turpis aliquam ullamcorper. Phasellus massa dui, congue ut mi at, accumsan auctor arcu. Quisque at mauris sagittis nisl lobortis rhoncus id ut quam. Maecenas eget enim sed enim viverra laoreet. Phasellus interdum turpis urna, ac aliquam metus faucibus ut. Nulla finibus ut lectus eget venenatis. Aenean eget ullamcorper nibh, quis fringilla diam. Sed sed neque vel velit tincidunt suscipit bibendum sit amet tellus. Sed efficitur quam vel leo faucibus cursus.
+            </section>
+            <section class="col-md-5">
+                Отзывы клиентов
+            </section>
+        </div>
+
+        <section class="text-center site-map">
+             Это мини карта сайта
+        </section>
     </div>
 </div>
 
 <footer class="footer">
     <div class="container">
         <div class="row">
-            <div class="col-md-7">
-                <p class="pull-left">&copy; sovetnik-gkh.ru <?= date('Y') ?></p>
+            <div class="col-md-7 text-left">
+                &copy; sovetnik-gkh.ru <?= date('Y') ?>
             </div>
-            <div class="col-md-5">
+            <div class="col-md-5 text-right">
                 <?=
                 Html::a(
                     Html::img('/img/social/VK.png', ['alt' => Yii::t('app', 'Вконтакте'), 'width' => 30, 'height' => 30]),
