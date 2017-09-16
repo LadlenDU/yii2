2446f34adf930c1da53989313dcb88c84c4b86dd
Ladlen <TwilightTower@mail.ru>
2017-09-16 17:21:32 +0300

    ЛК левое меню

--- frontend/views/layouts/manager/left.php
+++ frontend/views/layouts/manager/left.php
@@ -3,19 +3,19 @@
     <section class="sidebar">
 
         <!-- Sidebar user panel -->
-        <div class="user-panel">
+        <!--<div class="user-panel">
             <div class="pull-left image">
-                <img src="<?= $directoryAsset ?>/img/user2-160x160.jpg" class="img-circle" alt="User Image"/>
+                <img src="<? /*= $directoryAsset */ ?>/img/user2-160x160.jpg" class="img-circle" alt="User Image"/>
             </div>
             <div class="pull-left info">
                 <p>Alexander Pierce</p>
 
                 <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
             </div>
-        </div>
+        </div>-->
 
         <!-- search form -->
-        <form action="#" method="get" class="sidebar-form">
+        <!--<form action="#" method="get" class="sidebar-form">
             <div class="input-group">
                 <input type="text" name="q" class="form-control" placeholder="Search..."/>
               <span class="input-group-btn">
@@ -23,20 +23,45 @@
                 </button>
               </span>
             </div>
-        </form>
+        </form>-->
         <!-- /.search form -->
 
         <?= dmstr\widgets\Menu::widget(
             [
                 'options' => ['class' => 'sidebar-menu'],
                 'items' => [
-                    ['label' => 'Меню Yii2', 'options' => ['class' => 'header']],
-                    ['label' => 'Структура', 'icon' => 'share-alt', 'url' => ['/site']],
-                    ['label' => 'Страницы', 'icon' => 'file-code-o', 'url' => ['/pages/manager']],
-                    ['label' => 'RBAC', 'icon' => 'users', 'url' => ['/rbac']],
-                    ['label' => 'Gii', 'icon' => 'file-code-o', 'url' => ['/gii']],
-                    ['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug']],
-                    ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
+                    //['label' => 'Меню Yii2', 'options' => ['class' => 'header']],
+                    ['label' => 'Рабочий стол', 'icon' => 'share-alt', 'url' => ['/manager/desctop']],
+                    ['label' => 'Моя организация', 'icon' => 'file-code-o', 'url' => ['/manager/my-organization']],
+                    ['label' => 'Организации', 'icon' => 'users', 'url' => ['/manager/organizations']],
+                    ['label' => 'Дома', 'icon' => 'file-code-o', 'url' => ['/manager/buildings']],
+                    [
+                        'label' => 'Должники',
+                        'icon' => 'dashboard',
+                        //'url' => ['/manager/debtors'],
+                        'url' => ['#'],
+                        'items' => [
+                            ['label' => 'Проверка регистрации', 'icon' => 'file-code-o', 'url' => ['/manager/debtors/verify-registration'],],
+                            ['label' => 'Проверка задолженности', 'icon' => 'dashboard', 'url' => ['/manager/debtors/debt-verification'],],
+                            ['label' => 'Расчет пошлины', 'icon' => 'dashboard', 'url' => ['/manager/debtors/calculation-of-duty'],],
+                        ],
+                    ],
+                    [
+                        'label' => 'Заявление в суд',
+                        'icon' => 'dashboard',
+                        'url' => ['#'],
+                        'items' => [
+                            ['label' => 'Курьер', 'icon' => 'file-code-o', 'url' => ['/manager/court/courier'],],
+                        ],
+                    ],
+                    ['label' => 'Заявление приставам', 'icon' => 'file-code-o', 'url' => ['/manager/statement-to-bailiffs']],
+                    ['label' => 'Рассылки', 'icon' => 'file-code-o', 'url' => ['/manager/newsletters']],
+                    ['label' => 'Отчеты', 'icon' => 'file-code-o', 'url' => ['/manager/reports']],
+                    ['label' => 'Аналитика', 'icon' => 'file-code-o', 'url' => ['/manager/analytics']],
+                    ['label' => 'Настройка', 'icon' => 'file-code-o', 'url' => ['/manager/customize']],
+                    ['label' => 'Новости', 'icon' => 'file-code-o', 'url' => ['/manager/news']],
+                    ['label' => 'Панель управления', 'icon' => 'file-code-o', 'url' => ['/manager/control-panel']],
+                    /*['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
                     [
                         'label' => 'Same tools',
                         'icon' => 'share',
@@ -62,7 +87,7 @@
                                 ],
                             ],
                         ],
-                    ],
+                    ],*/
                 ],
             ]
         ) ?>
