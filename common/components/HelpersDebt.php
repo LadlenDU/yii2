7d1f4f5e1320137c05c0c79a2f53618353d392f2
Ladlen <TwilightTower@mail.ru>
2017-09-20 23:22:36 +0300

    генерация Invoice для должника

--- common/components/HelpersDebt.php
+++ common/components/HelpersDebt.php
@@ -27,12 +27,32 @@ class HelpersDebt
         $xls->setActiveSheetIndex(0);
         $sheet = $xls->getActiveSheet();
 
-        // наименование получателя
-        $sheet->setCellValueByColumnAndRow(16, 3, $court->name);
+        // наименование получателя платежа
+        $sheet->setCellValueByColumnAndRow(16, 3, $court->name_of_payee);
+
         // ИНН получателя платежа
         $sheet->setCellValueByColumnAndRow(16, 5, $court->INN);
         // номер счета получателя платежа
         $sheet->setCellValueByColumnAndRow(36, 5, $court->beneficiary_account_number);
+
+        // наименование банка получателя платежа
+        $sheet->setCellValueByColumnAndRow(16, 7, $court->beneficiary_bank_name);
+        // БИК
+        $sheet->setCellValueByColumnAndRow(52, 7, $court->BIC);
+
+        // Номер кор./сч. банка получателя платежа
+        $sheet->setCellValueByColumnAndRow(39, 9, $court->KBK);
+
+        // наименование платежа
+        $sheet->setCellValueByColumnAndRow(16, 10, 'Оплата госпошлины');
+        // номер лицевого счета (код) плательщика
+        $sheet->setCellValueByColumnAndRow(50, 10, $court->OKTMO);
+
+        // Ф.И.О. плательщика
+        $sheet->setCellValueByColumnAndRow(26, 12, $debtor->getFIOName());
+        // Адрес плательщика
+        $sheet->setCellValueByColumnAndRow(26, 13, $debtor->getFullAddress());
+
 
         header("Expires: Mon, 1 Apr 1974 05:00:00 GMT");
         header("Last-Modified: " . gmdate("D,d M YH:i:s") . " GMT");
