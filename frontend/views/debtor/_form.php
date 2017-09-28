f16bab28373e92ad90805af174a6a4aebd2220c2
Ladlen <TwilightTower@mail.ru>
2017-09-28 18:37:04 +0300

    выбор главного менеджера

--- frontend/views/debtor/_form.php
+++ frontend/views/debtor/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use common\models\GeneralManager;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Debtor */
@@ -42,7 +44,9 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'privatized')->textInput() ?>
 
-    <?= $form->field($model, 'general_manager_id')->textInput() ?>
+    <? /*= $form->field($model, 'general_manager_id')->textInput() */ ?>
+
+    <?= $form->field($model, 'general_manager_id')->dropDownList(ArrayHelper::map(GeneralManager::find()->all(), 'id', 'second_name'), ['prompt' => '']) ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Создать') : Yii::t('app', 'Обновить'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
