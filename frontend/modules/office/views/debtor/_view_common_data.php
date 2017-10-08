f1958fea5596f2c05af94047614c16392f8f9d4b
Ladlen <TwilightTower@mail.ru>
2017-10-08 23:26:07 +0300

    debtor view fixes by Andrey's comments

--- frontend/modules/office/views/debtor/_view_common_data.php
+++ frontend/modules/office/views/debtor/_view_common_data.php
@@ -23,21 +23,21 @@ $attributes = [
         'columns' => [
             [
                 'attribute' => 'name_id',
-                'label' => $modelName->getAttributeLabel('first_name'),
-                'value' => $fm->asText($modelName->first_name),
-                'labelColOptions' => ['style' => 'width:10.3%;text-align:right'],
-                'valueColOptions' => ['style' => 'width:23%'],
-                'displayOnly' => true,
-                'format'=>'raw',
-            ],
-            [
-                'attribute' => 'name_id',
                 'label' => $modelName->getAttributeLabel('second_name'),
                 'value' => $fm->asText($modelName->second_name),
                 'labelColOptions' => ['style' => 'width:10.3%;text-align:right'],
                 'valueColOptions' => ['style' => 'width:23%'],
                 'displayOnly' => true,
-                'format'=>'raw',
+                'format' => 'raw',
+            ],
+            [
+                'attribute' => 'name_id',
+                'label' => $modelName->getAttributeLabel('first_name'),
+                'value' => $fm->asText($modelName->first_name),
+                'labelColOptions' => ['style' => 'width:10.3%;text-align:right'],
+                'valueColOptions' => ['style' => 'width:23%'],
+                'displayOnly' => true,
+                'format' => 'raw',
             ],
             [
                 'attribute' => 'name_id',
@@ -46,27 +46,28 @@ $attributes = [
                 'labelColOptions' => ['style' => 'width:10.3%;text-align:right'],
                 'valueColOptions' => ['style' => 'width:23%'],
                 'displayOnly' => true,
-                'format'=>'raw',
+                'format' => 'raw',
             ],
         ],
     ],
-    [
+    /*[
         'columns' => [
             [
                 'attribute' => 'name_id',
                 'label' => Yii::t('app', 'Имя полностью'),
                 'value' => $fm->asText($modelName->full_name),
                 'displayOnly' => true,
-                'format'=>'raw',
+                'format' => 'raw',
             ],
         ],
-    ],
+        'rowOptions' => ['class' => 'warning', 'style' => 'border-top: 5px double #dedede'],
+    ],*/
     [
         'group' => true,
         'label' => Yii::t('app', 'Адрес'),
         'rowOptions' => ['class' => 'info']
     ],
-    [
+    /*[
         'columns' => [
             [
                 'attribute' => 'location_id',
@@ -127,17 +128,18 @@ $attributes = [
                 'format' => 'raw',
             ],
         ],
-    ],
+    ],*/
     [
         'columns' => [
             [
                 'attribute' => 'location_id',
-                'label' => Yii::t('app', 'Адрес полностью'),
+                'label' => Yii::t('app', 'Полный адрес'),
                 'value' => $fm->asText($modelLocation->arbitraty),
                 'displayOnly' => true,
                 'format' => 'raw',
             ],
         ],
+        //'rowOptions' => ['class' => 'warning', 'style' => 'border-top: 5px double #dedede'],
     ],
     [
         'group' => true,
@@ -147,34 +149,26 @@ $attributes = [
     [
         'columns' => [
             [
-                'attribute' => 'phone',
-                'displayOnly' => true,
-            ],
-        ],
-    ],
-    [
-        'columns' => [
-            [
                 'attribute' => 'LS_EIRC',
                 'value' => '<kbd>' . $fm->asText($model->LS_EIRC) . '</kbd>',
-                'labelColOptions' => ['style' => 'width:10.3%;text-align:right'],
-                'valueColOptions' => ['style' => 'width:23%'],
+                'labelColOptions' => ['style' => 'width:20%;text-align:right'],
+                'valueColOptions' => ['style' => 'width:30%'],
                 'displayOnly' => true,
                 'format' => 'raw',
             ],
-            [
+            /*[
                 'attribute' => 'LS_IKU_provider',
                 'value' => '<kbd>' . $fm->asText($model->LS_IKU_provider) . '</kbd>',
                 'labelColOptions' => ['style' => 'width:10.3%;text-align:right'],
                 'valueColOptions' => ['style' => 'width:23%'],
                 'displayOnly' => true,
                 'format' => 'raw',
-            ],
+            ],*/
             [
                 'attribute' => 'IKU',
                 'value' => '<kbd>' . $fm->asText($model->IKU) . '</kbd>',
-                'labelColOptions' => ['style' => 'width:10.3%;text-align:right'],
-                'valueColOptions' => ['style' => 'width:23%'],
+                'labelColOptions' => ['style' => 'width:20%;text-align:right'],
+                'valueColOptions' => ['style' => 'width:30%'],
                 'displayOnly' => true,
                 'format' => 'raw',
             ],
@@ -183,20 +177,26 @@ $attributes = [
     [
         'columns' => [
             [
+                'attribute' => 'phone',
+                'labelColOptions' => ['style' => 'width:10.3%;text-align:right'],
+                'valueColOptions' => ['style' => 'width:23%'],
+                'displayOnly' => true,
+            ],
+            [
                 'attribute' => 'space_common',
                 'labelColOptions' => ['style' => 'width:10.3%;text-align:right'],
                 'valueColOptions' => ['style' => 'width:23%'],
                 'displayOnly' => true,
             ],
-            [
+            /*[
                 'attribute' => 'space_living',
                 'labelColOptions' => ['style' => 'width:10.3%;text-align:right'],
                 'valueColOptions' => ['style' => 'width:23%'],
                 'displayOnly' => true,
-            ],
+            ],*/
             [
                 'attribute' => 'ownership_type_id',
-                'value' => '<kbd>' . $fm->asText($modelOwnership->name) . '</kbd>',
+                'value' => $fm->asText($modelOwnership->name),
                 'labelColOptions' => ['style' => 'width:10.3%;text-align:right'],
                 'valueColOptions' => ['style' => 'width:23%'],
                 'displayOnly' => true,
@@ -208,7 +208,7 @@ $attributes = [
 
 echo DetailView::widget([
     'model' => $model,
-    'condensed' => true,
+    //'condensed' => true,
     'hover' => true,
     'bordered' => true,
     'striped' => false,
