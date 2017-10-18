5fd53524dd68bac6f3d405fd07a957b19b42e164
Ladlen <TwilightTower@mail.ru>
2017-10-19 01:10:42 +0300

    'nullDisplay' for frontend

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -15,6 +15,9 @@ return [
         'request' => [
             'csrfParam' => '_csrf-frontend',
         ],
+        'formatter' => [
+            'nullDisplay' => '-',
+        ],
         /*'user' => [
             'identityClass' => 'common\models\User',
             'enableAutoLogin' => true,
