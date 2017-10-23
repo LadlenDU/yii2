4ceb6115711bf541a10012f8d5343e902cb8ac76
Ladlen <TwilightTower@mail.ru>
2017-10-23 22:03:54 +0300

    caijq4ever/yii2-pdf added

--- common/config/main.php
+++ common/config/main.php
@@ -12,16 +12,16 @@ return [
             'class' => 'yii\caching\FileCache',
         ],
         'html2pdf' => [
-            'class' => 'yii2tech\html2pdf\Manager',
+            /*'class' => 'yii2tech\html2pdf\Manager',
             'viewPath' => '@app/pdf',
-            'converter' => 'wkhtmltopdf',
-            /*'class' => 'junqi\pdf\Pdf',
+            'converter' => 'wkhtmltopdf',*/
+            'class' => 'junqi\pdf\Pdf',
             'tmpDir' => '@runtime/pdf/',
 //            'options' => [
 //                'headerLeft' => 'world,hello',
 //                'headerLine' => true,
 //                //more options see `wkhtmltopdf -H`
-//            ],*/
+//            ],
         ],
         /*'html2pdf' => [
             'class' => 'yii2tech\html2pdf\Manager',
