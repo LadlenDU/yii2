c06932de1d1a0eb8f042837f9b3a3a122e4b6ccb
Ladlen <TwilightTower@mail.ru>
2017-10-26 11:20:32 +0300

    _full_report_fine_data text changes

--- frontend/modules/office/views/debtor/_full_report_fine_data.php
+++ frontend/modules/office/views/debtor/_full_report_fine_data.php
@@ -11,19 +11,19 @@
 
 <table style="width: 100%;margin:1em 0;">
     <tr>
-        <th colspan="2"><?= Yii::t('app','Расчёт пени по коммунальным платежам') ?></th>
+        <th colspan="2"><?= Yii::t('app','Расчет пени за коммунальрные услуги (ст. 155 ЖК РФ)') ?></th>
     </tr>
     <tr>
         <td><?= Yii::t('app','№ ЛС') ?></td>
         <td><?= $debtorLS ?></td>
     </tr>
     <tr>
-        <td><?= Yii::t('app','ФИО') ?></td>
-        <td><?= $debtorName ?></td>
+        <td><?= Yii::t('app','Адрес:') ?></td>
+        <td><?= $debtorAddress ?></td>
     </tr>
     <tr>
-        <td><?= Yii::t('app','Адрес') ?></td>
-        <td><?= $debtorAddress ?></td>
+        <td><?= Yii::t('app','Должник:') ?></td>
+        <td><?= $debtorName ?></td>
     </tr>
 </table>
 
