a21fd018db48002ec741c5f45cb374bee71b73f6
Ladlen <TwilightTower@mail.ru>
2017-10-26 06:46:34 +0300

    custom FancytreeAsset - not used though

---
+++ frontend/assets/FancytreeAsset.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace frontend\assets;
+
+use wbraganca\fancytree\FancytreeAsset as FancytreeAssetBase;
+
+class FancytreeAsset extends FancytreeAssetBase
+{
+    public $skin = 'dist/skin-win8/ui.fancytree';
+}
