f856a930b436a8f614149e7f0f097d3ae9343d49
Ladlen <TwilightTower@mail.ru>
2017-11-06 01:18:41 +0300

    debt time restriction correction

--- common/models/DebtDateRestrictionTrait.php
+++ common/models/DebtDateRestrictionTrait.php
@@ -7,7 +7,7 @@ trait DebtDateRestrictionTrait
     protected function addDateRestriction($column)
     {
         $time = new \DateTime('now');
-        $topTime = $time->modify('-3 years')->modify('+1 month')->format('Y-m-d');
+        $topTime = $time->modify('-3 years -1 month')->format('Y-m-d');
         $this->andWhere(['>=', $column, $topTime]);
     }
 }
