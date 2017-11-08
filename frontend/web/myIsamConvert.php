7ef65b766d3ab2b8305253fba000171fa3d0be07
Ladlen <TwilightTower@mail.ru>
2017-11-08 16:32:41 +0300

    myIsam converter

---
+++ frontend/web/myIsamConvert.php
@@ -0,0 +1,24 @@
+<?php
+
+echo 'conversion_started<br>';
+
+mysql_connect('localhost', 'root', 'temp123');
+
+mysql_select_db('sgkh');
+
+// Actual code starts here
+
+$sql = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES
+        WHERE TABLE_SCHEMA = 'sgkh' 
+        AND ENGINE = 'MyISAM'";
+
+$rs = mysql_query($sql);
+
+while($row = mysql_fetch_array($rs))
+{
+    $tbl = $row[0];
+    $sql = "ALTER TABLE `$tbl` ENGINE=INNODB";
+    mysql_query($sql);
+}
+
+echo 'converted';
